3116ad9a05d3cb1bfe52f1c7a20a5e3ac96fc1e4
Jose Molina <josemolinaf22@gmail.com>
2022-06-04 05:37:14 -0600

    initial push

---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Card() {
+  //THIS CONNECTS TO TTHE APP.JS
+  return <div>This is the Card component</div>;
+}
+
+export default Card;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "../components/Header.module.css";
+
+function Header() {
+  //THIS IS CONNECTED TO THE APP.js
+  return <div className={styles.header}>Header</div>;
+}
+
+export default Header;
